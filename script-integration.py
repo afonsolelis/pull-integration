0927188ab444bb2e69b1a8445823021712db2d35
afonsolelis <afonsolelis@gmail.com>
2023-10-29 19:34:45 -0300

    ok

--- script-integration.py
+++ script-integration.py
@@ -12,7 +12,6 @@ def create_table():
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
 
-    # Verificar e criar a tabela "sales"
     cur.execute("""
     CREATE TABLE IF NOT EXISTS sales (
         id INTEGER PRIMARY KEY,
@@ -24,7 +23,6 @@ def create_table():
     )
     """)
 
-    # Verificar e criar a tabela "category"
     cur.execute("""
     CREATE TABLE IF NOT EXISTS category (
         id INTEGER PRIMARY KEY,
@@ -45,10 +43,7 @@ def return_response():
 
 @app.route('/update', methods=['GET'])
 def update_data():
-    # Obtém a data do dia anterior
     sale_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-
-    # Chama o endpoint para obter os dados
     url = "https://intelifunctiongetdata.azurewebsites.net/api/InteliFunctionGetData"
     params = {
         "code": "pZh3gmJW_87epswrWDuB7CvQle-KqjsVh2ZJUaifiXd4AzFuOEy98w==",
@@ -63,21 +58,21 @@ def update_data():
     if response.status_code == 200:
         data = response.json()
 
-        # Conectar ao banco de dados
+        # Check if 'sales' and 'category' are present in data
+        if not isinstance(data, dict) or 'sales' not in data or 'category' not in data:
+            return jsonify({"status": "error", "message": "Invalid data format from the endpoint."}), 500
+
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
 
-        # Atualiza a tabela de vendas
         for sale in data['sales']:
             cur.execute("INSERT INTO sales (id, amount, saleDate, cnpj, idCategory, value) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO UPDATE SET amount = %s, saleDate = %s, cnpj = %s, idCategory = %s, value = %s",
                         (sale['id'], sale['amount'], sale['saleDate'], sale['cnpj'], sale['idCategory'], sale['value'], sale['amount'], sale['saleDate'], sale['cnpj'], sale['idCategory'], sale['value']))
 
-        # Atualiza a tabela de categoria (assumindo que os dados da categoria estão em data['category'])
         for category in data['category']:
             cur.execute("INSERT INTO category (id, isActive, name) VALUES (%s, %s, %s) ON CONFLICT (id) DO UPDATE SET isActive = %s, name = %s",
                         (category['id'], category['isActive'], category['name'], category['isActive'], category['name']))
 
-        # Commitar as mudanças e fechar a conexão
         conn.commit()
         cur.close()
         conn.close()
@@ -85,7 +80,6 @@ def update_data():
         return jsonify({"status": "success", "message": "Data updated successfully!"}), 200
     else:
         return jsonify({"status": "error", "message": "Failed to fetch data from the endpoint."}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
