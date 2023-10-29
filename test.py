6b22cbe480f4a2b50b10ac8b96b8f4439fe032b8
afonsolelis <afonsolelis@gmail.com>
2023-10-29 20:06:43 -0300

    ok

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 from flask import Flask, jsonify
 import requests
 from datetime import datetime, timedelta
-import psycopg2
+import sqlite3
 
 app = Flask(__name__)
 
-DATABASE_URL = 'postgres://afonsolelis:b5On1h1hhzLHQJ0mLp4IC3jzbETkW4Aw@dpg-ckvd70ramefc73fibotg-a/integration_6jsy'
+DATABASE_FILE = 'local_database.db'
 
 @app.route('/createtable', methods=['GET'])
 def create_table():
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = sqlite3.connect(DATABASE_FILE)
     cur = conn.cursor()
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS sales (
         id INTEGER PRIMARY KEY,
         amount INTEGER,
-        saleDate DATE,
+        saleDate TEXT,
         cnpj TEXT,
         idCategory INTEGER,
         value REAL
@@ -57,22 +57,23 @@ def update_data():
     if response.status_code == 200:
         data = response.json()
 
+
         if not isinstance(data, list):
             return jsonify({"status": "error", "message": "Invalid data format from the endpoint."}), 500
 
-        conn = psycopg2.connect(DATABASE_URL)
+        conn = sqlite3.connect(DATABASE_FILE)
         cur = conn.cursor()
 
-        added_categories = set()  # Para garantir que cada categoria seja adicionada apenas uma vez
+        added_categories = set() # Para garantir que cada categoria seja adicionada apenas uma vez
 
         for sale in data:
-            cur.execute("INSERT INTO sales (id, amount, saleDate, cnpj, idCategory, value) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO UPDATE SET amount = %s, saleDate = %s, cnpj = %s, idCategory = %s, value = %s",
-                        (sale['id'], sale['amount'], sale['saleDate'], sale['cnpj'], sale['idCategory'], sale['value'], sale['amount'], sale['saleDate'], sale['cnpj'], sale['idCategory'], sale['value']))
+            cur.execute("INSERT OR REPLACE INTO sales (id, amount, saleDate, cnpj, idCategory, value) VALUES (?, ?, ?, ?, ?, ?)",
+                        (sale['id'], sale['amount'], sale['saleDate'], sale['cnpj'], sale['idCategory'], sale['value']))
 
             category = sale['category']
             if category['id'] not in added_categories:
-                cur.execute("INSERT INTO category (id, isActive, name) VALUES (%s, %s, %s) ON CONFLICT (id) DO UPDATE SET isActive = %s, name = %s",
-                            (category['id'], category['isActive'], category['name'], category['isActive'], category['name']))
+                cur.execute("INSERT OR REPLACE INTO category (id, isActive, name) VALUES (?, ?, ?)",
+                            (category['id'], category['isActive'], category['name']))
                 added_categories.add(category['id'])
 
         conn.commit()
